20d1ebd697ec725ff07a17afde3340a1ef636412
Md. Iftiar Hossain <developer.iftiarhossain@gmail.com>
2023-12-26 21:21:21 +0600

    create

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,17 @@
+import { Col, Container, Row } from "react-bootstrap";
+
 const Header = () => {
   return (
     <>
       <header>
         <h1>I am from Header</h1>
+        <Container>
+          <Row>
+            <Col md={6}>
+              <h2>The header Area</h2>
+            </Col>
+          </Row>
+        </Container>
       </header>
     </>
   );
